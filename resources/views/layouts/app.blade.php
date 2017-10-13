908b8ae2396f41cad7455b6dfcbf5ebd41e6a4cc
mkhsojib <mkhsojib@gmail.com>
2017-10-13 23:56:00 +0600

    compilling assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,12 +3,35 @@
   <head>
     <meta charset="utf-8">
     <title>Basic Web Site</title>
+
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-@yield('content')
 
-@include('inc.sidebar')
+@include('inc.navbar')
+<div class="container">
 
+@if(Request::is('/'))
+
+  @include('inc.showcase')
+
+  @endif
+    <div class="row">
+      <div class="col-md-8 col-lg-8">
+    @yield('content')
+      </div>
+
+      <div class="col-md-4 col-lg-4">
+    @include('inc.sidebar')
+      </div>
+    </div>
+</div>
+
+<footer id="footer" class="text-center">
+
+<p>Copyright 2017 &copy; Basic Web site</p>
+
+</footer>
 
   </body>
 </html>
