ed183912f5dc36e389a874770cfd21d9d1e5eb66
mkhsojib <mkhsojib@gmail.com>
2017-10-15 22:22:15 +0600

    Forms With Laravel Collective

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,12 +2,28 @@
 @section('content')
 <h1>Contact Page</h1>
 
-@endsection
+{!! Form::open(['url' => 'contact/submit']) !!}
+
+<div class="form-group">
+{{ Form::label('name', 'Name') }}
+{{ Form::text('name', '' , ['class'=> 'form-control', 'placeholder'=>'name here']) }}
+</div>
+
+<div class="form-group">
+{{ Form::label('email', 'E-Mail Address') }}
+{{ Form::text('email', '' , ['class'=> 'form-control', 'placeholder'=>'email here'] ) }}
+</div>
+
+<div class="form-group">
+{{ Form::label('message', 'Enter Message') }}
+{{ Form::textarea('message', '', ['class'=> 'form-control', 'placeholder'=>'message here']) }}
+</div>
 
 
+<div class="">
+  {{ Form::submit('Submit', ['class'=>'btn btn-primary'])}}
+</div>
 
-@section('sidebar')
-@parent
-
+{!! Form::close() !!}
 
 @endsection
